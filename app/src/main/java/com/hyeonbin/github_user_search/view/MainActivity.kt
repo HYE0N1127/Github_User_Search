3dc342b0a0a9affff738c55c081405d4c8c24e61
현빈 <hhb041127@naver.com>
2024-01-19 17:29:47 +0900

    Add: Design XML, ViewBinding 연결 작업

--- app/src/main/java/com/hyeonbin/github_user_search/view/MainActivity.kt
+++ app/src/main/java/com/hyeonbin/github_user_search/view/MainActivity.kt
@@ -3,10 +3,13 @@ package com.hyeonbin.github_user_search.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeonbin.github_user_search.R
+import com.hyeonbin.github_user_search.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
