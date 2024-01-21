24bef1ffe819445a834711ee2c2d1644211b202a
현빈 <hhb041127@naver.com>
2024-01-21 13:38:48 +0900

    Feat : API에서 유저를 검색한 결과를 받아오는 로직 추가

--- app/src/main/java/com/hyeonbin/github_user_search/view/MainActivity.kt
+++ app/src/main/java/com/hyeonbin/github_user_search/view/MainActivity.kt
@@ -2,14 +2,43 @@ package com.hyeonbin.github_user_search.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hyeonbin.github_user_search.R
+import android.view.inputmethod.EditorInfo
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.hyeonbin.github_user_search.databinding.ActivityMainBinding
+import com.hyeonbin.github_user_search.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.etSearchUser.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                // Editable -> String 형 병환
+                val searchQuery = binding.etSearchUser.text.toString()
+                viewModel.searchUserList(searchQuery)
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private suspend fun initCollector() = lifecycleScope.launch(Dispatchers.Main) {
+        with(viewModel) {
+            recommendUserFlow.collectLatest {
+                // TODO(어댑터 연결 로직 추가)
+            }
+        }
     }
 }
--- app/src/main/java/com/hyeonbin/github_user_search/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/hyeonbin/github_user_search/viewmodel/MainViewModel.kt
@@ -1,4 +1,35 @@
 package com.hyeonbin.github_user_search.viewmodel
 
-class MainViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hyeonbin.github_user_search.entity.RecommendUser
+import com.hyeonbin.github_user_search.entity.RecommendUserItem
+import com.hyeonbin.github_user_search.repository.RecommendUserRepository
+import com.hyeonbin.github_user_search.repository.impl.RecommendUserRepositoryImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val repository: RecommendUserRepository by lazy { RecommendUserRepositoryImpl() }
+
+    // Data Binding Variable
+    private val _searchUserQuery: MutableStateFlow<String> = MutableStateFlow("")
+    val searchUserQuery: StateFlow<String> = _searchUserQuery.asStateFlow()
+
+    // Server Networking Variable
+    private val _recommendUserFlow: MutableStateFlow<RecommendUser> = MutableStateFlow(RecommendUser(0, true, emptyList()))
+    val recommendUserFlow: StateFlow<RecommendUser> = _recommendUserFlow.asStateFlow()
+
+    fun searchUserList(searchQuery: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getRecommendUser(searchQuery).collect {
+                Log.d("Test5", "$it")
+//                _recommendUserFlow.emit(it)
+            }
+        }
+    }
 }
