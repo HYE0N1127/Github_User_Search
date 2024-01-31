7c0eef3cedb30f39c1234ef68df9b29c7b212004
현빈 <hhb041127@naver.com>
2024-01-31 17:35:38 +0900

    Add: Repository Class 추가

--- app/src/main/java/com/hyeonbin/github_user_search/repository/UserRepository.kt
+++ app/src/main/java/com/hyeonbin/github_user_search/repository/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.hyeonbin.github_user_search.repository
 
 import com.hyeonbin.github_user_search.entity.recommend_user.RecommendUser
+import com.hyeonbin.github_user_search.entity.user.User
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun getRecommendUser(userName: String): Flow<RecommendUser>
+    suspend fun getUserDetail(userName: String): Flow<User>
 }
