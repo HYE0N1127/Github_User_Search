a77270238b39ff3f58633dd7a2b11a6f9dfa8b5d
현빈 <hhb041127@naver.com>
2024-01-31 17:34:39 +0900

    Modify: Rename recommendUserService -> userService

--- app/src/main/java/com/hyeonbin/github_user_search/repository/impl/UserRepositoryImpl.kt
+++ app/src/main/java/com/hyeonbin/github_user_search/repository/impl/UserRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.hyeonbin.github_user_search.repository.impl
 
 import com.hyeonbin.github_user_search.entity.recommend_user.RecommendUser
+import com.hyeonbin.github_user_search.entity.user.User
 import com.hyeonbin.github_user_search.mapper.toEntity
 import com.hyeonbin.github_user_search.network.client.RetrofitClient
 import com.hyeonbin.github_user_search.repository.UserRepository
@@ -8,9 +9,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class UserRepositoryImpl : UserRepository {
-    private val recommendUserService = RetrofitClient.getUserService()
+    private val userService = RetrofitClient.getUserService()
     override suspend fun getRecommendUser(userName: String): Flow<RecommendUser> = flow {
-        val response = recommendUserService.searchRecommendUser(userName)
+        val response = userService.searchRecommendUser(userName)
         val emptyData = RecommendUser(0, true, emptyList())
         if (response.isSuccessful) {
             when {
@@ -19,13 +20,29 @@ class UserRepositoryImpl : UserRepository {
                 }
 
                 response.body() != null -> {
-                    val value = response.body()!!
-                    emit(value.toEntity())
+                    val value = response.body()!!.toEntity()
+                    emit(value)
                 }
             }
         } else {
             // TODO(에러 상황에서 빈 리스트가 아닌 특정 에러를 알려줄 만한 방안이 필요함)
             emit(emptyData)
+        }
+    }
+
+    override suspend fun getUserDetail(userName: String): Flow<User> = flow {
+        val response = userService.getDetailUserInfo(userName)
+        if (response.isSuccessful) {
+            if (response.body() == null) {
+                val errorData = User("", 0, "", "", "", false, 0, 0, "", "", "", isError = true, errorMessage = "값이 비어있습니다.")
+                emit(errorData)
+            } else {
+                val value = response.body()!!.toEntity()
+                emit(value)
+            }
+        } else {
+            val errorData = User("", 0, "", "", "", false, 0, 0, "", "", "", isError = true, errorMessage = "오류가 발생하였습니다.")
+            emit(errorData)
         }
     }
 }
